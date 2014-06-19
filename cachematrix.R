aae63e6274b73e532783e68bbbbc444a3ac2689f
ottojolanki <otaljo@gmail.com>
2014-06-18 17:49:54 -0700

    working functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## In this file are provided two functions. makeCacheMatrix, which creates 
+## a matrix capable to cache its inverse matrix. The actual calculation and
+## the(possible) caching is done in cacheSolve -function.
 
-## Write a short comment describing this function
+
+## The makeCacheMatrix creates a special matrix, which is a list containing
+## a function to
+## 1.set the value of the matrix
+## 2.get the value of the matrix
+## 3.set the value of the inverse of the matrix
+## 4.get the value of the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+      inv <- NULL
+      set <- function(y){
+            x <<- y
+            inv <<- NULL
+      }
+      get <- function() x
+      setinv <- function(inverse) inv<<-inverse
+      getinv <- function() inv
+      list(set = set, get = get, setinv = setinv, getinv = getinv)
 
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve calculates the inverse of a special matrix created by
+## the makeCacheMatrix -function. It first checks if the said inverse 
+## is calculated. If so, it gets the mean from the cache, skips the computation
+## and returns the inverse from cache, if not it calculates the inverse, and
+## returns it.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      ## Return a matrix that is the inverse of 'x'
+      inv<-x$getinv()
+      ## Check if the cached value is not NULL, and if so, return it
+      ## with message telling that a cached value is returned
+      if(!is.null(inv)){
+            message("Getting cached data.")
+            return(inv)
+      }
+      ## So, the inverse was not cached and must be calculated. Lastly the 
+      ## calculated value is going be set the cached value using setinv 
+      ## -function, and returned.
+      data <- x$get()
+      inv <- solve(data)
+      x$setinv(inv)
+      return(inv)
 }
